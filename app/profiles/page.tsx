20389b897800bee012d67d5b3571fc19e2127318
ProdFilthi <155994707+ProdFilthi@users.noreply.github.com>
2025-02-15 19:48:25 +0200

    add profile info

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,9 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="mb-[500px] flex items-center justify-center flex-col space-y-8">
+    <div className="mb-[500px] pt-24 flex items-center justify-center flex-col space-y-8">
       <h1 className="text-4xl">Profiles</h1>
+      <div className="flex items-center justify-center gap-8">
+        <Image
+          src="/filthidp.jpg"
+          width={3024}
+          height={4032}
+          alt="profiledp"
+          className="w-[100px] h-[100px] object-cover rounded-full border cursor-pointer"
+        />
+        <div>
+          <h1>Filthi</h1>
+          <h1>Programmer Filthi</h1>
+          <h1 className="flex items-center gap-1">
+            Online <span className="text-[8px]">🟢</span>
+          </h1>
+        </div>
+      </div>
+      <button
+        type="button"
+        className="bg-gray-600 w-40 h-10 cursor-pointer rounded-md active:opacity-80"
+      >
+        Edit Profile
+      </button>
     </div>
   );
 };
